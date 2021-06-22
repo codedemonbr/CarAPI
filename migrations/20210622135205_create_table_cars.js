f77279743998a8b8fec1b93918a1faf1fa473759
Henrique <henrique.thsantos.ths@gmail.com>
2021-06-22 11:27:24 -0300

    DB Migrations

--- migrations/20210622135205_create_table_cars.js
+++ migrations/20210622135205_create_table_cars.js
@@ -1,8 +1,14 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable("cars", (table) => {
+        table.increments("id").primary();
+        table.string("brand").notNull(); // Marca
+        table.string("model").notNull(); // Modelo
+        table.integer("year").notNull(); // Ano
+        table.string("fuel").notNull(); // Combustível
+        table.decimal("price").notNull(); //Preço
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTable("cars");
 };
